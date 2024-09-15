009297ca5d49a00c3cbacfd8d7e6dc18edb1b011
Sergei Vinogradov <vinser52@gmail.com>
2024-09-15 23:53:51 +0200

    Add llama.cpp example

---
+++ llama_cpp_example/main.py
@@ -0,0 +1,26 @@
+from llama_cpp import Llama
+
+def get_llm():
+    llm = Llama.from_pretrained(
+        repo_id="bartowski/Meta-Llama-3.1-8B-Instruct-GGUF",
+        filename="*Q8_0.gguf",
+        verbose=True
+    )
+
+    return llm
+
+def run_example():
+    llm = get_llm()
+    output = llm(
+        "Q: Write a simple hello world example in C++ A: ", # Prompt
+        max_tokens=32, # Generate up to 32 tokens, set to None to generate up to the end of the context window
+        stop=["Q:", "\n"], # Stop generating just before the model would generate a new question
+        echo=True # Echo the prompt back in the output
+    ) # Generate a completion, can also call create_completion
+    
+    print("Model output: ")
+    print(output)
+
+
+if __name__ == "__main__":
+    run_example()
