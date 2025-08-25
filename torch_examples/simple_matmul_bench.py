4483eac54498ec00d7ef85032d6866006f3c6423
Sergei Vinogradov <vinser52@gmail.com>
2025-08-26 00:10:23 +0200

    Enable XPU in the simple_matmul_bench

--- torch_examples/simple_matmul_bench.py
+++ torch_examples/simple_matmul_bench.py
@@ -1,7 +1,17 @@
 import torch
 import time
 
-def benchmark(device, size=20000, steps=10):
+def synchronize(device):
+    if device.type == "cuda":
+        torch.cuda.synchronize()
+    elif device.type == "xpu":
+        torch.xpu.synchronize()
+    elif device.type == "mps":
+        torch.mps.synchronize()
+    elif device.type == "cpu":
+        torch.cpu.synchronize()
+
+def benchmark(device, size=10000, steps=10):
     print(f"\nRunning on {device}...")
     a = torch.randn(size, size, device=device)
     b = torch.randn(size, size, device=device)
@@ -10,12 +20,12 @@ def benchmark(device, size=20000, steps=10):
     for _ in range(3):
         torch.matmul(a, b)
 
-    torch.cuda.synchronize() if device.type == "cuda" else None
+    synchronize(device)
 
     start = time.time()
     for _ in range(steps):
         c = torch.matmul(a, b)
-    torch.cuda.synchronize() if device.type == "cuda" else None
+    synchronize(device)
     end = time.time()
 
     print(f"Time per step: {(end - start) / steps:.4f} seconds")
@@ -28,4 +38,10 @@ if torch.backends.mps.is_available():
     benchmark(torch.device("mps"))
 else:
     print("\nMPS not available on this system.")
+
+# Intel GPU (XPU) if available
+if torch.xpu.is_available():
+    benchmark(torch.device("xpu"))
+else:
+    print("\nIntel GPU (XPU) not available on this system.")
 
